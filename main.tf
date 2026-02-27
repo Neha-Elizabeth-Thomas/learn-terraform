231925e48271a5665e11c041d59385f6b6f571b0
Neha-Elizabeth-Thomas <148973334+Neha-Elizabeth-Thomas@users.noreply.github.com>
2026-02-27 11:12:30 +0530

    Add Terraform configuration for random pet and local file

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "random"{}
+
+resource "random_pet" "name"{
+  length=2
+}
+
+output "pet_name"{
+  value=random_pet.name.id
+}
+
+resource "local_file" "sample"{
+  filename=var.filename
+  content="This is a sample file"
+}
